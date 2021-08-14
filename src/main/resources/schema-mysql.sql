2819559768f6c309f6118edf318ca42e5803fdb3
elyesntc <61362019+elyesntc@users.noreply.github.com>
2021-08-14 15:00:42 +0100

    Delete schema-mysql.sql

--- src/main/resources/schema-mysql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS etudiants (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    prenom VARCHAR(100) NOT NULL,
-    nom VARCHAR(100) NOT NULL,
-    departement VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
